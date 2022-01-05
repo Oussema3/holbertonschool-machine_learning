082bc63c137b928d0b90d897bd680a71504c86b4
Oussema3 <ldrajmioussema2@gmail.com>
2022-01-05 11:25:28 +0000

    still on matrix multipmication

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" function that performs matrix multiplication"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """ uses numpy's .matmul operation """
+    return np.matmul(mat1, mat2)
