b75dafb3bc29c0418d7f2ac5d2e358a047a96865
Oussema3 <ldrajmioussema2@gmail.com>
2022-01-05 10:49:03 +0000

    concenating arrays

---
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" function that concatenates two arrays"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+    used list method to add
+    returns concat_list
+    """
+    concat_list = list(arr1) + arr2
+    return concat_list
