8373213b9c506bc795324aa2c0d2613bbe0018cb
Oussema3 <ldrajmioussema2@gmail.com>
2022-01-05 11:19:18 +0000

    buchrangers transpose

---
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" function that transposes matrix"""
+
+
+def np_transpose(matrix):
+    """ uses transpose method in numpy """
+    import numpy as np
+    return np.transpose(matrix)
