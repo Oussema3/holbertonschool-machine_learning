2fcc670ca762e2f8884198a2765e54a16c08dbb2
Oussema3 <ldrajmioussema2@gmail.com>
2022-01-05 10:32:15 +0000

    sizing function

---
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Calculate the shape of a matrixand return a list of integers """
+
+
+def matrix_shape(matrix):
+    """
+    calculatie size of matrix where matrix is
+    input and returns len of matrix if true
+    """
+    if not matrix:
+        return None
+    if type(matrix[0]) is not list:
+        return [len(matrix)]
+    return [len(matrix)] + matrix_shape(matrix[0])
