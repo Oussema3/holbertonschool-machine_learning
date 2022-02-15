7d4fdcaa003ae37a0cebbe8bf6932371b15cb1ad
Oussema3 <ldrajmioussema2@gmail.com>
2022-02-16 00:37:14 +0100

    blank

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -6,4 +6,4 @@ def summation_i_squared(n):
     """Calculate summation of i^2 from 1 to n"""
     if type(n) is not int or n < 1:
         return None
-    return int(n / 6 + n * n / 2 + pow(n, 3) / 3)
+    return int(n / 6 + n * n / 2 + pow(n, 3) / 3)     
