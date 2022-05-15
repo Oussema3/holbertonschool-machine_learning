58f73267e2d61ecb22935169d57080a2f9c75f08
Oussema3 <ldrajmioussema2@gmail.com>
2022-05-15 02:59:59 +0000

    updating

--- unsupervised_learning/0x01-clustering/2-variance.py
+++ unsupervised_learning/0x01-clustering/2-variance.py
@@ -1,27 +1,37 @@
 #!/usr/bin/env python3
 """
-Defines function that calculates total intra-cluster variance for a data set
+    Variance
+    ***
+    Intra-cluster distance: the distance between members
+    of a cluster.
+    ***
 """
-
-
 import numpy as np
 
 
 def variance(X, C):
     """
-    Calculates the total intra-cluster variance for a data set
-    parameters:
-        X [numpy.ndarray of shape (n, d)]:
-            contains the dataset used for K-means clustering
-            n: the number of data points
-            d: the number of dimensions for each data point
-        C [numpy.ndarray of shape (k, d)]:
-            contains the centroid means for each cluster
-            k: the number of clusters
-            d: the number of dimensions for each data point
-    should not use any loops
-    returns:
-        var [float]: total variance
+    Method to calculate the total intra-cluster variance
+    for a data set.
+    Parameters:
+        X (numpy.ndarray of shape(n, d)):
+        containing the data set
+            n (int): number of data points.
+            d (int): number of dimensions for each data point.
+        C (numpy.ndarray of shape (k, d)):
+        containing the centroid means for each cluster.
+    Returns:
+        var (float): the total variance
         or None on failure
     """
-    return None
+    # https://stats.stackexchange.com/questions/86645/variance-within-each-cluster
+    # https://numpy.org/doc/stable/reference/generated/numpy.apply_along_axis.html
+    # https://numpy.org/doc/stable/reference/generated/numpy.subtract.html
+    if (not isinstance(C, np.ndarray)) or (len(X.shape) != 2):
+        return None
+    if (not isinstance(C, np.ndarray)) or (len(C.shape) != 2):
+        return None
+
+    sub = np.apply_along_axis(np.subtract, 1, X, C)
+    # X(250x2) C(5x2) => (250x5x2)
+    return (np.square(sub).sum(axis=2).min(axis=1).sum())
