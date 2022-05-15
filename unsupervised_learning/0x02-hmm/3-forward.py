d9bb1a6804302c25b72b8321ab7c804aac2fa083
Oussema3 <ldrajmioussema2@gmail.com>
2022-05-15 05:46:42 +0000

    forwarding

--- unsupervised_learning/0x02-hmm/3-forward.py
+++ unsupervised_learning/0x02-hmm/3-forward.py
@@ -1,53 +1,31 @@
 #!/usr/bin/env python3
-"""
-    Hidden Markov Models - The Forward Algorithm
-    Goal: joint distribution of Z_k and X_1:k
-"""
+"""Function that performs the forward algorithm for a hidden markov model"""
+
 import numpy as np
 
 
 def forward(Observation, Emission, Transition, Initial):
-    """
-    Method to perform the forward algorithm for a hidden markov model.
-    Parameters:
-        Observation (numpy.ndarray of shape (T,)):
-         contains the index of the observation
-         - T is the number of observations
-        Emission (numpy.ndarray of shape (N, M)):
-         containing the emission probability of a specific observation given
-         a hidden state
-            - Emission[i, j] is the probability of observing j given the hidden
-            state i
-             - N: the number of hidden states.
-             - M: the number of all possible observations.
-        Transition (2D numpy.ndarray of shape (N, N)):
-         containing the transition probabilities
-            - Transition[i, j] is the probability of transitioning from the
-            hidden state i to j
-        Initial (numpy.ndarray of shape (N, 1)):
-         containing the probability of starting in a particular hidden state.
-    Returns: P, F, or None, None on failure
-        P: the likelihood of the observations given the model
-        F (numpy.ndarray of shape (N, T)): containing the forward path
-         probabilities
-            - F[i, j] is the probability of being in hidden state i at time j
-            given the previous observations.
-    """
-
+    """Function that performs the forward
+    algorithm for a hidden markov model"""
+    if type(Observation) is not np.ndarray or len(Observation.shape) != 1:
+        return (None, None)
+    if type(Emission) is not np.ndarray or len(Emission.shape) != 2:
+        return (None, None)
+    if type(Transition) is not np.ndarray or len(Transition.shape) != 2:
+        return (None, None)
+    if type(Initial) is not np.ndarray or len(Initial.shape) != 2:
+        return (None, None)
     T = Observation.shape[0]
     N, M = Emission.shape
-
-    forward = np.zeros((N, T))
-
-    for s in range(N):
-        forward[s, 0] = Initial[s] * Emission[s, Observation[0]]
-    # or => forward[:, 0] = Initial.T * Emission[:, Observation[0]]
-    for t in range(1, T):
-        for s in range(N):
-            # T_mul_E = Transition[:, s] * Emission[s, Observation[t]]
-            sum_mul =np.matmul(forward[:, t - 1], Transition[:, s])
-            forward[s, t] = sum_mul * Emission[s, Observation[t]]
-
-    P = np.sum(forward[:, T - 1])
-
-    return P, forward
+    if Transition.shape[0] != Transition.shape[1] or Transition.shape[0] != N:
+        return (None, None)
+    if N != Initial.shape[0] or Initial.shape[1] != 1:
+        return (None, None)
+    F = np.zeros([N, T])
+    F[:, 0] = Initial.T * Emission[:, Observation[0]]
+    for i in range(1, T):
+        for j in range(N):
+            F[j, i] = F[:, i - 1].dot(Transition[:, j]) *\
+                Emission[j, Observation[i]]
+    P = np.sum(F[:, T - 1:], axis=0)[0]
+    return (P, F)
